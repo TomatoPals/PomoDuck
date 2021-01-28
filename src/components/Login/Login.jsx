9506ca67e43bba20d37585b75b8eba44c9465472
mcereal <mpbcurtis@gmail.com>
2021-01-28 09:50:06 -0500

    adding eslint and config

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,8 +3,11 @@ import axios from "axios";
 const BASEURL = "http://localhost:3001/api/login/";
 
 const userLogin = {
-  userLogin: (email, password) =>
-    axios.post(BASEURL, { email: email, password: password }).then((res) => res)
+  userLogin: (email, password) => {
+    axios
+      .post(BASEURL, { email: email, password: password })
+      .then((res) => res);
+  }
 };
 
 export default userLogin;
