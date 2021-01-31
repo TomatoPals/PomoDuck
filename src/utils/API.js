6825a8bf43d50bd0825de7c34f7eb9abdc2b924e
mcereal <mpbcurtis@gmail.com>
2021-01-30 19:31:25 -0500

    updating deplopyment yaml

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,14 +2,13 @@ import axios from "axios";
 
 // const BASEURL = "http://localhost:3001/api/login/";
 const DEVURL = "http://localhost";
-// const PRODURL = process.env.PROD_URL;
-const API_ROUTE = "/api/login/";
+// const PRODURL = process.env.EXPRESS_SERVICE;
+const API_ROUTE = "/api/user/";
 const PORT = "3001";
 
 const userLogin = {
   userLogin: async (email, password) => {
     const BASEURL = `${DEVURL}:${PORT}${API_ROUTE}`;
-    console.log(BASEURL);
     try {
       const request = await axios.post(BASEURL, {
         email: email,
