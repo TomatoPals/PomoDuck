e755f5b43ce72e280a73923442052e5b0ae0c52a
G Strick <graysonstricker@gmail.com>
2021-02-01 13:48:11 -0500

    add Modal to login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "../src/pages/NoMatch/NoMatch";
 import HomePage from "../src/pages/HomePage/HomePage";
+import Login from "../src/components/Login/Login";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
           <Route exact path="/" component={HomePage} />
           <Route component={NoMatch} />
         </Switch>
+        <Route exact path="/Login" component={Login} />
       </div>
     </Router>
   );
