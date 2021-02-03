5681106f2992d98d3a076b5d0f2e6d4eb4ce507d
G Strick <graysonstricker@gmail.com>
2021-02-03 18:04:22 -0500

    add in connection to settings modal

--- src/components/MenuAppBar/MenuAppBar.jsx
+++ src/components/MenuAppBar/MenuAppBar.jsx
@@ -3,11 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import "./MenuAppBar.css";
 import SimpleModal from "../Modal/SimpleModal";
+import SettingsModal from "../Modal/SettingsModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,7 +41,8 @@ export default function NavBar() {
             PomoDuck
           </Typography>
           {/* Get login button to open login modal */}
-          <Button color="inherit">Login</Button>
+          <SettingsModal />
+          <SimpleModal />
         </Toolbar>
       </AppBar>
     </div>
