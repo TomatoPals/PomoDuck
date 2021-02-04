21f0780016c75b702c58eac0e56fb6ce0a1ea3cf
G Strick <graysonstricker@gmail.com>
2021-02-04 18:02:07 -0500

    update tasks

---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Tasks = () => {
+  return (
+    <>
+      <article className="topLayer">
+        <div className="container">
+          <div className="timer"></div>
+          <div className="tasks">
+            <h2>Tasks</h2>
+          </div>
+          <div className="taskbox">
+            <p>Add Task</p>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default Tasks;
