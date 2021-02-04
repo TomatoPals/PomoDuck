06125f4d2d4f98dd0797c395a8ec78934f338e15
G Strick <graysonstricker@gmail.com>
2021-02-04 18:01:56 -0500

    update menu bar with logo

--- src/components/MenuAppBar/MenuAppBar.jsx
+++ src/components/MenuAppBar/MenuAppBar.jsx
@@ -26,25 +26,35 @@ export default function NavBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            PomoDuck
-          </Typography>
-          {/* Get login button to open login modal */}
-          <SettingsModal />
-          <SimpleModal />
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <>
+          <div className={classes.root}>
+            <nav className="navbar">
+              <div className="container">
+                <div className="logo">
+                  <img src="/Assets/pomoduck-logo.svg" alt="logo" />
+                  <img src="/Assets/pomoduck-duck.svg" alt="duck icon" />
+                </div>
+              </div>
+            </nav>
+          </div>
+        </>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          PomoDuck
+        </Typography>
+        {/* Get login button to open login modal */}
+        <SettingsModal />
+        <SimpleModal />
+      </Toolbar>
+    </AppBar>
   );
 }
