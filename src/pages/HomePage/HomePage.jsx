c22e79ee8f65891e97f31bff73c59ba5e27337f9
G Strick <graysonstricker@gmail.com>
2021-02-04 18:02:50 -0500

    update homepage with new components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,18 @@
 import React from "react";
 import "./HomePage.css";
 // import Login from "../../components/Login/Login";
-import CircularIndeterminate from "../../components/Spinners/LoadingSpinner";
-
+import Tasks from "../../components/Tasks/Tasks";
 import MenuAppBar from "../../components/MenuAppBar/MenuAppBar";
 // import CreateUser from "../../components/CreateUser/CreateUser";
+import Instructions from "../../components/Instructions/Instructions";
 
 const HomePage = () => {
   return (
     <>
       <MenuAppBar />
       {/* <Login /> */}
-      <CircularIndeterminate />
-      <nav className="navbar">
-        <div className="container">
-          <div className="logo">
-            <img src="/Assets/pomoduck-logo.svg" alt="logo" />
-            <img src="/Assets/pomoduck-duck.svg" alt="duck icon" />
-          </div>
-        </div>
-      </nav>
-      <article className="topLayer">
-        <div className="container">
-          <div className="timer"></div>
-          <div className="tasks">
-            <h2>Tasks</h2>
-          </div>
-          <div className="taskbox">
-            <p>Add Task</p>
-          </div>
-        </div>
-      </article>
-      <div className="lowerbox"></div>
-      <div className="container">
-        <div className="instructions">
-          <h2>Stay focused on the important things in life. </h2>
-          <h3>What is PomoDuck?</h3>
-          {/* <p>Pomoduck is a customizable pomodoro timer that works on desktop and mobile browser. The aim of this app is to help you focus on any task you are working on, such as study, writing, or coding. This app is inspired by Pomodoro Technique which is a time management method developed by Francesco Cirillo.</p> */}
-        </div>
-      </div>
+      <Tasks />
+      <Instructions />
       {/* <CreateUser /> */}
     </>
   );
