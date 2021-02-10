5f55909a249f8a35b4e4f63bf39bcb8546cfab36
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2021-02-09 20:07:26 -0500

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import "../../assets/styles/styles.css";
 const Footer = () => {
   return (
     <>
-      <Grid container className="footer" alignItems="center" justify="center">
+      <Grid container className="footer" alignItems="center" justify="center" spacing={2}>
         <Grid item xs={1}>
           <h3>Home</h3>
         </Grid>
@@ -13,20 +13,20 @@ const Footer = () => {
           <h3>Contact</h3>
         </Grid>
       </Grid>
-      <Grid container alignItems="center" justify="center">
+      <Grid container alignItems="center" justify="center" spacing={3}>
         <Grid item xs={1}>
           <a href="#" target="_blank" rel="noopener">
-            <img src="/assets/images/icons/face.svg" alt="facebook icon" />
+            <img src="/Assets/icons/face.svg" alt="facebook icon" />
           </a>
         </Grid>
         <Grid item xs={1}>
           <a href="#" target="_blank" rel="noopener">
-            <img src="/assets/images/icons/twit.svg" alt="facebook icon" />
+            <img src="/Assets/icons/twit.svg" alt="facebook icon" />
           </a>
         </Grid>
         <Grid item xs={1}>
           <a href="#" target="_blank" rel="noopener">
-            <img src="/assets/images/icons/in.svg" alt="facebook icon" />
+            <img src="/Assets/icons/in.svg" alt="facebook icon" />
           </a>
         </Grid>
       </Grid>
