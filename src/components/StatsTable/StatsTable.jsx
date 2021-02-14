b08d3214765bf809c75b8f13ac41d5b7631676c5
mcereal <mpbcurtis@gmail.com>
2021-02-13 19:29:42 -0500

    showing user stats in console

--- src/components/StatsTable/StatsTable.jsx
+++ src/components/StatsTable/StatsTable.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import CreateStatsTable from "../CreateStatsTable/CreateStatsTable";
-import {Grid } from "@material-ui/core";
-
+import { Grid } from "@material-ui/core";
 
 const StatsTable = (props) => {
-    console.log('stats table props ', props)
-    return(
-        <>
-        {/* <Grid> */}
-            {/* {console.log(props)} */}
-            <CreateStatsTable stats={props.stat}/>
-        {/* </Grid> */}
-        </>
-    )
-}
+  return (
+    <>
+      {/* <Grid> */}
+      {/* {console.log(props)} */}
+      <CreateStatsTable stats={props.stat} />
+      {/* </Grid> */}
+    </>
+  );
+};
 export default StatsTable;
