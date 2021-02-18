7542460cd2a584b1db75d66d20dc3db944463b94
G Strick <graysonstricker@gmail.com>
2021-02-17 22:58:01 -0500

    fix bug in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
-import { AppBar, Toolbar, ButtonGroup } from "@material-ui/core";
+import { AppBar, Toolbar, Grid, ButtonGroup } from "@material-ui/core";
 import React from "react";
 import SignUpModal from "../Modal/SignUpModal";
 import SettingsModal from "../../components/Modal/SettingsModal";
 import StatsButton from "../../components/StatsButton/StatsButton";
-
 import IconButton from "@material-ui/core/IconButton";
 import PomoDuckHome from "./PomoDuckHome";
-
 const Header = (props) => {
   return (
     // // <AppBar position="relative" id="appbarStyles">
@@ -16,14 +14,15 @@ const Header = (props) => {
         <IconButton edge="start" color="inherit" aria-label="menu">
           <PomoDuckHome />
         </IconButton>
-        <ButtonGroup className="right-box">
-          <StatsButton id="signup" />
-          <SettingsModal className="settings" />
-          <SignUpModal id="signup" isLoggedin={props.isLoggedin} callback={props.callback} />
-        </ButtonGroup>
+        <Grid container justify="flex-end">
+          <ButtonGroup>
+            <StatsButton id="signup" />
+            <SettingsModal className="settings" />
+            <SignUpModal id="signup" isLoggedin={props.isLoggedin} callback={props.callback} />
+          </ButtonGroup>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
 };
-
 export default Header;
