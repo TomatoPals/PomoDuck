aea33ccf67e7a244eba32024fd08baba1111a803
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2021-02-18 20:56:47 -0500

    updated buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Grid, ButtonGroup } from "@material-ui/core";
+import { AppBar, Toolbar, ButtonGroup } from "@material-ui/core";
 import React from "react";
 import SignUpModal from "../Modal/SignUpModal";
 import SettingsModal from "../../components/Modal/SettingsModal";
@@ -14,13 +14,11 @@ const Header = (props) => {
         <IconButton edge="start" color="inherit" aria-label="menu">
           <PomoDuckHome />
         </IconButton>
-        <Grid container justify="flex-end">
-          <ButtonGroup>
-            <StatsButton id="signup" />
-            <SettingsModal className="settings" />
-            <SignUpModal id="signup" isLoggedin={props.isLoggedin} callback={props.callback} />
-          </ButtonGroup>
-        </Grid>
+        <ButtonGroup>
+          <StatsButton className="settings" />
+          <SettingsModal className="settings" />
+          <SignUpModal className="settings" isLoggedin={props.isLoggedin} callback={props.callback} />
+        </ButtonGroup>
       </Toolbar>
     </AppBar>
   );
