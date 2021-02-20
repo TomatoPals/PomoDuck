37a2f43ae9497a744be96b862d0f4900f5b0a3f1
mcereal <mpbcurtis@gmail.com>
2021-02-20 10:31:50 -0500

    prod deployment config

--- src/pages/StatsPage/StatsPage.jsx
+++ src/pages/StatsPage/StatsPage.jsx
@@ -11,62 +11,51 @@ const StatsPage = () => {
   //This useEffect gets all of the users info after they are logged in
   //you can see their total stats on breaks, pomodoros with this
   useEffect(() => {
-    console.log('test');
+    console.log("test");
     if (storeState.userInfo.loggedIn) {
       // console.log(storeState.userInfo.userDetails);
     }
   }, [storeState]);
 
-  const calcTotalLongBreak = () => {
-    console.log(storeState.userInfo.userDetails);
-    let longBreak = storeState.userInfo.userDetails.totalBigBreakSeconds;
-    return(longBreak);
-  };
+  const calcTotalLongBreak = () => storeState.userInfo.userDetails.totalBigBreakSeconds;
 
-  const calcTotalWork = () => {
-    let totalWork = storeState.userInfo.userDetails.totalPomSeconds;
-    return(totalWork);
-  }
+  const calcTotalWork = () => storeState.userInfo.userDetails.totalPomSeconds;
 
-  const calcShortBreak = () => {
-    let shortBreak = storeState.userInfo.userDetails.totalSmallBreakSeconds;
-    return(shortBreak);
-  }
+  const calcShortBreak = () => storeState.userInfo.userDetails.totalSmallBreakSeconds;
 
   return (
     <>
       {/* <Grid container direction="column"> */}
-        <Grid item>
-          <Header />
-          <Grid item sm={12}>
-            <StatsTable stat={storeState.taskList.tasks} />
-          </Grid>
-          <Grid container item sm={12} alignItems="center" justify="center"></Grid>
-          {/* <Button onClick={setStatState}>I'm Useless!</Button> */}
+      <Grid item>
+        <Header />
+        <Grid item sm={12}>
+          <StatsTable stat={storeState.taskList.tasks} />
         </Grid>
-        {/* <Grid item xs={12} sm={12} md={10} lg={8}> */}
-        <Chart
-          width={"500px"}
-          height={"500px"}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ["Works/Break", "Minutes per day"],
-            ["Long Breaks", calcTotalLongBreak()],
-            ["Short Breaks", calcShortBreak()],
-            ["Work", calcTotalWork()],
-          ]}
-          options={{
-            title: "Productivity",
-            // Just add this option
-            pieHole: 0.4
-          }}
-          rootProps={{ "data-testid": "3" }}
-        />
-        {/* </Grid> */}
+        <Grid container item sm={12} alignItems="center" justify="center"></Grid>
+        {/* <Button onClick={setStatState}>I'm Useless!</Button> */}
+      </Grid>
+      {/* <Grid item xs={12} sm={12} md={10} lg={8}> */}
+      <Chart
+        width={"500px"}
+        height={"500px"}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ["Works/Break", "Minutes per day"],
+          ["Long Breaks", calcTotalLongBreak()],
+          ["Short Breaks", calcShortBreak()],
+          ["Work", calcTotalWork()]
+        ]}
+        options={{
+          title: "Productivity",
+          // Just add this option
+          pieHole: 0.4
+        }}
+        rootProps={{ "data-testid": "3" }}
+      />
+      {/* </Grid> */}
       {/* </Grid> */}
       <Footer />
-
     </>
   );
 };
