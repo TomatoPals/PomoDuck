49bd18a9a28198b1ac520b6714037c29262aa602
mcereal <mpbcurtis@gmail.com>
2021-02-20 22:06:00 -0500

    updating prod config

--- src/utils/env-routes.js
+++ src/utils/env-routes.js
@@ -1,34 +1,34 @@
 module.exports = {
   development: {
     // user
-    signup: "localhost:3001/api/user/signup/",
-    getuser: "localhost:3001/api/user/getuser/",
-    remove: "localhost:3001/api/user/remove/",
-    update: "localhost:3001/api/user/update/",
-    updateprofile: "localhost:3001/api/user/updateprofile/",
-    login: "localhost:3001/api/userSession/login/",
+    signup: "http://localhost:3001/api/user/signup/",
+    getuser: "http://localhost:3001/api/user/getuser/",
+    remove: "http://localhost:3001/api/user/remove/",
+    update: "http://localhost:3001/api/user/update/",
+    updateprofile: "http://localhost:3001/api/user/updateprofile/",
+    login: "http://localhost:3001/api/userSession/login/",
     logout: "localhost:3001/api/userSession/logout/",
     // task
-    createtask: "localhost:3001/api/task/createtask/",
-    gettask: "localhost:3001/api/task/gettask/",
-    taskremove: "localhost:3001/api/task/taskremove/",
-    taskupdate: "localhost:3001/api/task/taskupdate/",
-    findtasks: "localhost:3001/api/task/findtasks/"
+    createtask: "http://localhost:3001/api/task/createtask/",
+    gettask: "http://localhost:3001/api/task/gettask/",
+    taskremove: "http://localhost:3001/api/task/taskremove/",
+    taskupdate: "http://localhost:3001/api/task/taskupdate/",
+    findtasks: "http://localhost:3001/api/task/findtasks/"
   },
   production: {
     // user
-    signup: "pomoduck.com/api/user/signup/",
-    getuser: "pomoduck.com/api/user/getuser/",
-    remove: "pomoduck.com/api/user/remove/",
-    update: "pomoduck.com/api/user/update/",
-    updateprofile: "pomoduck.com/api/user/updateprofile/",
-    login: "pomoduck.com/api/userSession/login/",
-    logout: "pomoduck.com/api/userSession/logout/",
+    signup: "https://pomoduck.com/api/user/signup/",
+    getuser: "https://pomoduck.com/api/user/getuser/",
+    remove: "https://pomoduck.com/api/user/remove/",
+    update: "https://pomoduck.com/api/user/update/",
+    updateprofile: "https://pomoduck.com/api/user/updateprofile/",
+    login: "https://pomoduck.com/api/userSession/login/",
+    logout: "https://pomoduck.com/api/userSession/logout/",
     // task
-    createtask: "pomoduck.com/api.task/createtask",
-    gettask: "pomoduck.com/api.task/gettask",
-    taskremove: "pomoduck.com/api.task/taskremove",
-    taskupdate: "pomoduck.com/api.task/taskupdate",
-    findtasks: "pomoduck.com/api.task/findtasks/"
+    createtask: "https://pomoduck.com/api.task/createtask",
+    gettask: "https://pomoduck.com/api.task/gettask",
+    taskremove: "https://pomoduck.com/api.task/taskremove",
+    taskupdate: "https://pomoduck.com/api.task/taskupdate",
+    findtasks: "https://pomoduck.com/api.task/findtasks/"
   }
 };
