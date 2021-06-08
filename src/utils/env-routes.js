7d49ad9177ef98cc5b2dfbc20cf3a44362ba430b
mcereal <mpbcurtis@gmail.com>
2021-06-07 21:40:51 -0400

    changing to develop

--- src/utils/env-routes.js
+++ src/utils/env-routes.js
@@ -1,19 +1,19 @@
 module.exports = {
   development: {
     // user
-    signup: "http://localhost:3001/api/user/signup/",
-    getuser: "http://localhost:3001/api/user/getuser/",
-    remove: "http://localhost:3001/api/user/remove/",
-    update: "http://localhost:3001/api/user/update/",
-    updateprofile: "http://localhost:3001/api/user/updateprofile/",
-    login: "http://localhost:3001/api/userSession/login/",
-    logout: "http://localhost:3001/api/userSession/logout/",
+    signup: "https://pomoduck-backend.herokuapp.com/api/user/signup/",
+    getuser: "https://pomoduck-backend.herokuapp.com/api/user/getuser/",
+    remove: "https://pomoduck-backend.herokuapp.com/api/user/remove/",
+    update: "https://pomoduck-backend.herokuapp.com/api/user/update/",
+    updateprofile: "https://pomoduck-backend.herokuapp.com/api/user/updateprofile/",
+    login: "https://pomoduck-backend.herokuapp.com/api/userSession/login/",
+    logout: "https://pomoduck-backend.herokuapp.com/api/userSession/logout/",
     // task
-    createtask: "http://localhost:3001/api/task/createtask/",
-    gettask: "http://localhost:3001/api/task/gettask/",
-    taskremove: "http://localhost:3001/api/task/taskremove/",
-    taskupdate: "http://localhost:3001/api/task/taskupdate/",
-    findtasks: "http://localhost:3001/api/task/findtasks/"
+    createtask: "https://pomoduck-backend.herokuapp.com/api/task/createtask/",
+    gettask: "https://pomoduck-backend.herokuapp.com/api/task/gettask/",
+    taskremove: "https://pomoduck-backend.herokuapp.com/api/task/taskremove/",
+    taskupdate: "https://pomoduck-backend.herokuapp.com/api/task/taskupdate/",
+    findtasks: "https://pomoduck-backend.herokuapp.com/api/task/findtasks/"
   },
   production: {
     // user
